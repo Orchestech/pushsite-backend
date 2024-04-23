2f1f8def04d86765ad2c3a4e1a846863362a0058
Papin Nikolai <nikolai@weirdcat.ru>
2024-04-23 18:03:48 +0300

    Initial commit

---
+++ src/test/java/ru/rukolf/pushsite/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rukolf.pushsite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
