31bddfe152a54d15f4c6d0f80beb8dd22c755767
Papin Nikolai <nikolai@weirdcat.ru>
2024-04-24 15:29:54 +0300

    Role Repository

---
+++ src/main/java/ru/rukolf/pushsite/demo/repositories/RoleRepository.java
@@ -0,0 +1,17 @@
+package ru.rukolf.pushsite.demo.repositories;
+import java.util.Optional;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ru.rukolf.pushsite.demo.entities.RoleEntity;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    
+    Optional<RoleEntity> findRoleEntityById(Long id);
+    Optional<RoleEntity> findByTitle(String title);
+    List<RoleEntity> findAllByOperator(Boolean operator);
+
+}
