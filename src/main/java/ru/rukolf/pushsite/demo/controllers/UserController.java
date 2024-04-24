03ed62a4702b52bae84b61ae6ddd1e806597367f
Papin Nikolai <nikolai@weirdcat.ru>
2024-04-25 02:10:10 +0300

    Began implementing login & jwt auth

--- src/main/java/ru/rukolf/pushsite/demo/controllers/UserController.java
+++ src/main/java/ru/rukolf/pushsite/demo/controllers/UserController.java
@@ -3,11 +3,14 @@ package ru.rukolf.pushsite.demo.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -74,4 +77,32 @@ public class UserController {
         ObjectMapper objectMapper = new ObjectMapper();
         return ResponseEntity.ok(objectMapper.writeValueAsString(userResponse));
     }
+
+    @GetMapping(value="/user/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<?> login(
+        @RequestParam String username,
+        @RequestParam String password) throws JsonProcessingException {
+
+            UserEntity userEntity = userRepository.findByUsername(username).orElse(null);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            if (userEntity == null)
+            {
+                return new ResponseEntity<String>("Account not found", HttpStatus.NOT_FOUND);
+            }
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String hashedPassword = encoder.encode(password);
+            boolean isMatch = encoder.matches(userEntity.getPassword(), hashedPassword);
+
+            if (!isMatch)
+            {
+                return new ResponseEntity<String>("Invalid password", HttpStatus.UNAUTHORIZED);
+            }
+
+            String token = userService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
+
+            return ResponseEntity.ok(null);
+        }
 }
